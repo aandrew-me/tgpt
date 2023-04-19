18249b60cf90fa525f7bb4641cc7fa7f791e9108
Andrew <66430340+aandrew-me@users.noreply.github.com>
2023-04-20 00:00:00 +0600

    Change naming for executables

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # For GNU Linux
-go build -o ./build/tgpt-linux-x64
-GOARCH=386 go build -o ./build/tgpt-linux-x86
+go build -o ./build/tgpt-linux-amd64
+GOARCH=386 go build -o ./build/tgpt-linux-i386
 
 # For Windows
-GOOS=windows GOARCH=amd64 go build -o ./build/tgpt-x64.exe
-GOOS=windows GOARCH=386 go build -o ./build/tgpt-x86.exe
+GOOS=windows GOARCH=amd64 go build -o ./build/tgpt-amd64.exe
+GOOS=windows GOARCH=386 go build -o ./build/tgpt-i386.exe
 
 # For MacOS
-GOOS=darwin GOARCH=amd64 go build -o ./build/tgpt-mac-x64
+GOOS=darwin GOARCH=amd64 go build -o ./build/tgpt-mac-amd64
 GOOS=darwin GOARCH=arm64 go build -o ./build/tgpt-mac-arm64
