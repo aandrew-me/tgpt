b25118cbd338cd373f021ebce3acfe45eeecbaab
Andrew <66430340+aandrew-me@users.noreply.github.com>
2023-04-24 11:55:38 +0600

    update version

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 )
 
 const letters string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-const VERSION = "1.2.0"
+const VERSION = "1.2.1"
 
 var bold = color.New(color.Bold)
 var boldGreen = color.New(color.Bold, color.FgGreen)
