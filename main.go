27614fee4f533467af7400ad04c0dc4e8eef8c67
Andrew <66430340+aandrew-me@users.noreply.github.com>
2023-10-02 21:29:16 +0300

    Update local version

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ import (
 	"github.com/olekukonko/ts"
 )
 
-const localVersion = "1.9.0"
+const localVersion = "1.9.1"
 
 var bold = color.New(color.Bold)
 var boldBlue = color.New(color.Bold, color.FgBlue)
