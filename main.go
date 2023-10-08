bd2f2bbbcd39d1e4513392adee21e27f08b8bd23
Andrew <66430340+aandrew-me@users.noreply.github.com>
2023-10-08 09:55:17 +0300

    Update for go package manager

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ import (
 	"github.com/olekukonko/ts"
 )
 
-const localVersion = "2.0.1"
+const localVersion = "2.0.2"
 
 var bold = color.New(color.Bold)
 var boldBlue = color.New(color.Bold, color.FgBlue)
