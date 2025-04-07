3359ee1d62507df0cc0d448775eac57b32929604
aandrew-me <aandrew.me@proton.me>
2025-04-07 19:35:01 +0300

    Refactoring

---
+++ src/structs/structs.go
@@ -0,0 +1,40 @@
+package structs
+
+type Params struct {
+	ApiModel     string
+	ApiKey       string
+	Provider     string
+	Temperature  string
+	Top_p        string
+	Max_length   string
+	Preprompt    string
+	ThreadID     string
+	Url          string
+	PrevMessages string
+}
+
+type ExtraOptions struct {
+	IsGetSilent       bool
+	IsGetWhole        bool
+	IsGetCommand      bool
+	IsNormal          bool
+	IsGetCode         bool
+	IsInteractive     bool
+	AutoExec		  bool
+}
+
+type CommonResponse struct {
+	ID      string `json:"id"`
+	Choices []struct {
+		Delta struct {
+			Content string `json:"content"`
+		} `json:"delta"`
+	} `json:"choices"`
+}
+
+type ImageParams struct {
+	Params
+	Height int
+	Width  int
+	Out    string
+}
