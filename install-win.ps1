b4891b5ae6084d452ca92419952233eb0424307f
1RaY-1 <78962948+1RaY-1@users.noreply.github.com>
2025-07-11 10:54:26 +0200

    Update install-win.ps1

--- install-win.ps1
+++ install-win.ps1
@@ -1,7 +1,12 @@
-# TGPT installer script for Windows OS
+# TGPT Installer Script for Windows 10/11
 
-# If receiving error "execution of scripts is disabled on this system", open powershell as admin and type:
-# Set-ExecutionPolicy RemoteSigned
+# If you receive "execution of scripts is disabled on this system" error while running this script,
+# In PowerShell (as Administrator) type:
+#   Set-ExecutionPolicy RemoteSigned
+
+# Once you no longer need to run any PowerShell scripts,
+# it's better to set Execution Policy back to 'Restricted' (for better security):
+#   Set-ExecutionPolicy Restricted
 
 #Requires -RunAsAdministrator
 $ErrorActionPreference = "Stop"
@@ -10,49 +15,66 @@ function Check-Command($cmdname){
     return [bool](Get-Command -Name $cmdname -ErrorAction SilentlyContinue)
 }
 
-$target_dir = 'C:\Program Files\TGPT' # <-- here we wanna store our executable file
-$prog_name = "tgpt.exe" # <-- this is how we wanna call our executable
+$target_dir = 'C:\Program Files\TGPT' # <-- Needed directory for the executable
+$prog_name = "tgpt.exe"               # <-- Needed executable filename
 
 # Check if system is AMD64 or I386
 if ( [System.Environment]::Is64BitOperatingSystem ){
     write "Downloading executable for: 64-bit OS"
     Invoke-WebRequest -URI "https://github.com/aandrew-me/tgpt/releases/latest/download/tgpt-amd64.exe" -OutFile "$PWD\$prog_name"
 }
-else{
+else {
     write "Downloading executable for: 32-bit OS"
     Invoke-WebRequest -URI "https://github.com/aandrew-me/tgpt/releases/latest/download/tgpt-i386.exe" -OutFile "$PWD\$prog_name"
 }
 
-# Move executable to 'C:\Program Files\TGPT' folder
-
-# (who knows, maybe someone has installed Windows on another partition)
-if (!(Test-Path -Path 'C:\')) {
+# Make sure the 'C:\' exists
+if ( !( Test-Path -Path 'C:\' ) ) {
     write "C:\ Doesn't exist"
     exit
 } 
 
 write "Installing TGPT in C:\Program Files\"
-Sleep 2
+Sleep 1
 # Create 'TGPT' folder in needed directory
-if (!(Test-Path -Path $target_dir)) {
+if ( !( Test-Path -Path $target_dir ) ) {
     New-Item -ItemType Directory -Path $target_dir
 }
-# If tgpt is already installed, will overwrite it
-if (Test-Path -Path $target_dir\$prog_name -PathType Leaf){
+# Remove existing tgpt to install the new version
+if ( Test-Path -Path $target_dir\$prog_name -PathType Leaf ){
     Remove-Item $target_dir\$prog_name -force
 }
+
+# Move executable to 'C:\Program Files\TGPT'
 Move-item -Path $PWD\$prog_name -destination $target_dir\$prog_name
 
-# And add it to PATH
+# Add TGPT to PATH
 write "Adding TGPT to the PATH"
-if (Check-Command -cmdname $prog_name){
+if ( Check-Command -cmdname $prog_name ){
     write "$prog_name is already in PATH"
 }
 else {
-    setx /M PATH "`$Env:PATH;$target_dir"
+
+
 }
 
-write-host "Done! Terminal GPT is installed in '$target_dir\'."
-#   Remove last dot from variable prog_name
-$new = $prog_name.Substring(0, $prog_name.lastIndexOf('.'))
-write-host "Now you can open cmd and type '$new -h' for help."
+# Read the complete machine PATH from the registry
+$machinePath = [System.Environment]::GetEnvironmentVariable('Path','Machine')
+
+# Check if it's already in PATH
+if ( $machinePath -notmatch [Regex]::Escape($target_dir) ) {
+    $newMachinePath = "$machinePath;$target_dir"
+
+    # Add TGPT
+    [System.Environment]::SetEnvironmentVariable(
+    'Path',
+    $newMachinePath,
+    'Machine'
+    )
+  # No need to start a new cmd/pwsh session to be able to use TGPT
+    $Env:PATH += ";$target_dir"
+}
+
+write "Complete!"
+$new = $prog_name.Substring(0, $prog_name.lastIndexOf('.')) # Remove last dot from 'prog_name'
+write "To start using it, simply type '$new -h'."
